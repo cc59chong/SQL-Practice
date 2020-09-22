0addcd2690c608972ca9f1c8530882369c0c7f61
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-09-21 22:37:51 -0700

    Advanced Join

---
+++ HackerRank_SQL/Placements.sql
@@ -0,0 +1,21 @@
+SELECT a.Name
+FROM (SELECT s.ID, s.Name, p.Salary
+ FROM Students s
+ JOIN Packages p ON s.ID = p.ID) AS a
+JOIN 
+ (SELECT f.ID, f.Friend_ID, p.Salary
+ FROM Friends f
+ JOIN Packages p ON f.Friend_ID = P.ID) AS b
+ON a.ID = b.ID
+WHERE a.Salary < b.Salary
+ORDER BY b.Salary
+
+
+
+SELECT s.Name
+FROM Students s 
+JOIN Friends f ON s.ID = f.ID
+JOIN Packages pf ON f.Friend_ID = pf.ID
+JOIN Packages ps ON s.ID = ps.ID
+WHERE pf.Salary > ps.Salary
+ORDER BY pf.Salary
