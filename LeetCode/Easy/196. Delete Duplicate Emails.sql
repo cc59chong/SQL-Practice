c1553d27438bcb498497ee6dd3c7714925009ccd
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-10-27 22:37:13 -0700

    SQL DELETE

---
+++ LeetCode/Easy/196. Delete Duplicate Emails.sql
@@ -0,0 +1,8 @@
+DELETE FROM Person
+WHERE Id NOT IN (SELECT MIN(Id) 
+                 FROM (SELECT * FROM Person) AS a
+                 GROUP BY Email);
+
+
+DELETE p1 FROM Person p1, Person p2
+WHERE p1.Email = p2.Email AND p1.Id > P2.Id;
