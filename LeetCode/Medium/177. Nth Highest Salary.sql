52d7579ad7c152d1932ddce4266ab9cf41da5486
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-10-29 22:50:16 -0700

    Add files via upload

---
+++ LeetCode/Medium/177. Nth Highest Salary.sql
@@ -0,0 +1,40 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN( SELECT 
+              DISTINCT Salary
+		  FROM (SELECT
+		            Id,
+					Salary,
+					DENSE_RANK() OVER (
+					    ORDER BY Salary DESC
+					) AS 'Rank'
+				FROM 
+				   Employee
+				) e
+		  WHERE N = e.RANK 
+  );
+END
+
+/*--------------------------------------------------------*/
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+RETURN ( SELECT 
+             DISTINCT salary
+         FROM Employee
+         ORDER BY salary DESC
+         LIMIT 1 OFFSET N
+   );
+END
+
+/*----------------------------------------------------------*/
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N - 1;
+   RETURN(SELECT 
+               DISTINCT Salary
+          FROM  Employee
+          ORDER BY Salary DESC
+          limit N, 1  
+   );
+END
