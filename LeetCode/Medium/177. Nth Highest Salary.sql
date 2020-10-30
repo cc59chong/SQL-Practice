d4eddc4c678dc0e0ab762e15502ed7b18ace5b1e
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2020-10-29 22:51:43 -0700

    Update 177. Nth Highest Salary.sql

--- LeetCode/Medium/177. Nth Highest Salary.sql
+++ LeetCode/Medium/177. Nth Highest Salary.sql
@@ -2,16 +2,16 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN( SELECT 
               DISTINCT Salary
-		  FROM (SELECT
-		            Id,
-					Salary,
-					DENSE_RANK() OVER (
-					    ORDER BY Salary DESC
-					) AS 'Rank'
-				FROM 
-				   Employee
-				) e
-		  WHERE N = e.RANK 
+	  FROM (SELECT
+		    Id,
+	            Salary,
+		    DENSE_RANK() OVER (
+			   ORDER BY Salary DESC
+		    ) AS 'Rank'
+	        FROM 
+		   Employee
+		) e
+	 WHERE N = e.RANK 
   );
 END
 
