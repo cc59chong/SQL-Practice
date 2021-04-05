b82886267a0eae7a210e949f76fbb2efea8e17f5
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-04-05 15:59:19 -0700

    Advanced Select *

--- HackerRank_SQL/New Companies.sql
+++ HackerRank_SQL/New Companies.sql
@@ -6,7 +6,7 @@ SELECT c.company_code,
        c.founder, 
 	   COUNT(DISTINCT l.lead_manager_code), 
 	   COUNT(DISTINCT s.senior_manager_code),
-       COUNT(DISTINCT m.manager_code), 
+           COUNT(DISTINCT m.manager_code), 
 	   COUNT(DISTINCT e.employee_code)
 FROM Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e 
 WHERE c.company_code = l.company_code
