44d430cb52e8516bdd71f2f99f8de43ad528d0de
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-04-06 15:03:56 -0700

    Advanced Join -- Medium

--- HackerRank_SQL/Symmetric Pairs.sql
+++ HackerRank_SQL/Symmetric Pairs.sql
@@ -5,6 +5,12 @@ GROUP BY f1.X, f1.Y
 HAVING COUNT(f1.X) > 1 OR f1.X < f1.Y
 ORDER BY f1.X
 
+SELECT f1.X, f1.Y FROM Functions f1
+INNER JOIN Functions f2 ON f1.X=f2.Y AND f1.Y=f2.X
+GROUP BY f1.X, f1.Y
+HAVING COUNT(f1.X)>1 or f1.X<f1.Y
+ORDER BY f1.X 
+
 /*The key is the HAVING line, with two conditions.
 
 The first condition in that line makes sure pairs with the same X and Y values
