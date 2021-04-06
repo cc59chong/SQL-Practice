75d783937e702441c2d0070991b953387d0d1a3d
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-04-05 23:18:39 -0700

    Update Placements.sql

--- HackerRank_SQL/Placements.sql
+++ HackerRank_SQL/Placements.sql
@@ -1,11 +1,10 @@
 SELECT a.Name
 FROM (SELECT s.ID, s.Name, p.Salary
- FROM Students s
- JOIN Packages p ON s.ID = p.ID) AS a
-JOIN 
- (SELECT f.ID, f.Friend_ID, p.Salary
- FROM Friends f
- JOIN Packages p ON f.Friend_ID = P.ID) AS b
+      FROM Students s
+      JOIN Packages p ON s.ID = p.ID) AS a
+JOIN (SELECT f.ID, f.Friend_ID, p.Salary
+      FROM Friends f
+      JOIN Packages p ON f.Friend_ID = P.ID) AS b
 ON a.ID = b.ID
 WHERE a.Salary < b.Salary
 ORDER BY b.Salary
