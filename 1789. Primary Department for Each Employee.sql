9f08f0a8fad9841988ca7332aa70eb23b77f77a6
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-04-11 21:17:59 -0700

    Delete 1789. Primary Department for Each Employee.sql

--- 1789. Primary Department for Each Employee.sql
+++
@@ -1,8 +0,0 @@
-SELECT employee_id, department_id 
-FROM Employee
-WHERE primary_flag = 'Y'
-UNION
-SELECT employee_id, department_id 
-FROM Employee 
-GROUP BY employee_id
-HAVING COUNT(employee_id) = 1
