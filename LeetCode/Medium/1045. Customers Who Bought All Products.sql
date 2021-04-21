565484343408e0ea2cc40113b8a3ac43d033a31a
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-04-20 17:40:56 -0700

    Add files via upload

---
+++ LeetCode/Medium/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,5 @@
+SELECT customer_id
+FROM Customer
+WHERE product_key IN (SELECT product_key FROM Product) /*in case a customer buys a product that is not within product table.*/
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
