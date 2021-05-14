1900dd337cd1f448dbf3c40dd8f7992025e47d3b
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-05-14 14:22:26 -0700

    Update 626. Exchange Seats.sql

--- LeetCode/Medium/626. Exchange Seats.sql
+++ LeetCode/Medium/626. Exchange Seats.sql
@@ -5,3 +5,10 @@ SELECT
      END) id,student
 FROM seat
 ORDER BY id;
+
+SELECT (CASE WHEN MOD(id,2) = 0 THEN id -1
+             WHEN MOD(id,2) !=0 AND id != (SELECT MAX(id) FROM seat) THEN id +1
+        ELSE id 
+        END)id, student
+FROM seat
+ORDER BY id
