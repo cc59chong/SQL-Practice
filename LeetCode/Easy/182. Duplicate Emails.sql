ac111ba20fd549b969991618d96b795d9d6eb0c0
M.C <55506640+cc59chong@users.noreply.github.com>
2023-12-29 14:57:47 -0800

    Update 182. Duplicate Emails.sql

--- LeetCode/Easy/182. Duplicate Emails.sql
+++ LeetCode/Easy/182. Duplicate Emails.sql
@@ -1,3 +1,11 @@
 SELECT DISTINCT a.Email
 FROM Person a, Person b
 WHERE a.Email = b.Email AND a.Id != b.Id;
+
+/*
+# Write your MySQL query statement below
+SELECT Email
+FROM Person
+GROUP BY email
+Having COUNT(id) > 1
+*/
