cd180bf5de23395fa1bdf522f268e8003635f035
M.C <55506640+cc59chong@users.noreply.github.com>
2023-12-30 22:06:06 -0800

    Update 183. Customers Who Never Order.sql

--- LeetCode/Easy/183. Customers Who Never Order.sql
+++ LeetCode/Easy/183. Customers Who Never Order.sql
@@ -3,3 +3,12 @@ FROM Customers
 WHERE Id NOT IN (SELECT c.Id 
                  FROM Customers c 
                  JOIN Orders o ON c.Id = o.CustomerId);
+
+/*
+# Write your MySQL query statement below
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o ON c.id = o.customerId
+GROUP BY c.id
+HAVING COUNT(o.id) < 1;
+*/
