01619c2f420fa8e5cba142109c1eee45d2f045a1
M.C <55506640+cc59chong@users.noreply.github.com>
2024-01-11 20:06:55 -0800

    Update 1251. Average Selling Price.sql

--- LeetCode/Easy/1251. Average Selling Price.sql
+++ LeetCode/Easy/1251. Average Selling Price.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT p.product_id, 
-       ROUND((SUM(p.price * u.units))/(SUM(u.units)), 2) AS average_price
+       IFNULL(ROUND((SUM(p.price * u.units))/(SUM(u.units)), 2), 0) AS average_price
 FROM Prices p
 JOIN UnitsSold u ON p.product_id = u.product_id
-WHERE u.purchase_date BETWEEN p.start_date AND p.end_date
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
