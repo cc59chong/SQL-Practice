65816504656f867021b9180035d9c25259efd504
M.C <55506640+cc59chong@users.noreply.github.com>
2024-01-11 23:03:18 -0800

    Update 1517. Find Users With Valid E-Mails.sql

--- LeetCode/Easy/1517. Find Users With Valid E-Mails.sql
+++ LeetCode/Easy/1517. Find Users With Valid E-Mails.sql
@@ -1,7 +1,26 @@
 SELECT *
 FROM Users
-WHERE mail REGEXP '^[a-z][a-z0-9._-]*@leetcode.com$';
+WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\\?com)?\\.com$'
 
+/*
+Let's break down the regex pattern step by step to clearly explain each part:
+
+^: Anchor the regex pattern to match from the start of the string.
+[A-Za-z]: Match any single uppercase or lowercase letter. The email prefix name must start with a letter.
+[A-Za-z0-9_.-]*: Match any number of characters following the first letter in the email prefix name. It includes letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'.
+@: Match the literal '@' character, which separates the prefix name and the domain.
+leetcode: Match the literal 'leetcode', which is part of the email domain.
+(?com)?: Make the sequence ?com optional in the email domain. Allows the pattern to match both '@leetcode.com' and '@leetcode?com'.
+. : Match the literal '.' character, which separates the 'leetcode' part from the 'com' part of the domain.
+com: Match the literal 'com' at the end of the email domain.
+$: Anchor the regex pattern to match until the end of the string.
+*/
+
+ /*
+SELECT *
+FROM Users
+WHERE mail REGEXP '^[a-z][a-z0-9._-]*@leetcode.com$';
+*/
 
 /*
 SELECT *
