d9fe614019f5effa498dc07a2178ef4109388d5c
M.C <55506640+cc59chong@users.noreply.github.com>
2024-03-23 22:40:33 -0700

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- LeetCode/Easy/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ LeetCode/Easy/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -3,3 +3,15 @@ FROM Visits v
 LEFT JOIN Transactions t ON v.visit_id = t.visit_id
 WHERE t.transaction_id IS NULL
 GROUP BY v.customer_id
+
+SELECT customer_id, COUNT(visit_id) AS count_no_trans 
+FROM Visits
+WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
+GROUP BY customer_id
+
+SELECT v.customer_id, 
+       SUM(CASE WHEN transaction_id IS NULL THEN 1 ELSE 0 END) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
