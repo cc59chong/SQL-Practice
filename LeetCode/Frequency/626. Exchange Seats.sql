93c8d68b0b48260c234c487e6890cc89ac320b7f
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-14 01:44:35 -0700

    Update 626. Exchange Seats.sql

--- LeetCode/Frequency/626. Exchange Seats.sql
+++ LeetCode/Frequency/626. Exchange Seats.sql
@@ -5,3 +5,13 @@ SELECT
      END) id,student
 FROM seat
 ORDER BY id;
+
+/*------------------------------------------------------------------------*/
+
+SELECT 
+      (CASE WHEN id%2!=0 AND id != (SELECT MAX(id) FROM Seat) THEN id+1
+            WHEN id%2=0 THEN id-1
+            ELSE id
+        END) AS id, student
+FROM Seat
+ORDER BY id
