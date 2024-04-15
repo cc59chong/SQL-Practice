38da323579824196384af49cb0ad01addb77abfd
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-14 23:23:04 -0700

    Update 182. Duplicate Emails.sql

--- LeetCode/Frequency/182. Duplicate Emails.sql
+++ LeetCode/Frequency/182. Duplicate Emails.sql
@@ -1,3 +1,7 @@
 SELECT DISTINCT a.Email
 FROM Person a, Person b
 WHERE a.Email = b.Email AND a.Id != b.Id;
+
+SELECT DISTINCT email
+FROM Person
+WHERE email IN (SELECT email FROM Person GROUP BY email HAVING COUNT(id)>1);
