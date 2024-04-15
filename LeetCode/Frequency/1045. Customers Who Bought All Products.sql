9920d61b0ac1c8f6637697cee7e1cade07461082
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-15 10:21:25 -0700

    Update 1045. Customers Who Bought All Products.sql

--- LeetCode/Frequency/1045. Customers Who Bought All Products.sql
+++ LeetCode/Frequency/1045. Customers Who Bought All Products.sql
@@ -1,5 +1,12 @@
 SELECT customer_id
 FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
+
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+SELECT customer_id
+FROM Customer
 WHERE product_key IN (SELECT product_key FROM Product) /*in case a customer buys a product that is not within product table.*/
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
