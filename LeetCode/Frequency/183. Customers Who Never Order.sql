6bb37eaeb00e59c8914c0ca6ac68fd75b9fedf92
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-16 11:27:05 -0700

    Update 183. Customers Who Never Order.sql

--- LeetCode/Frequency/183. Customers Who Never Order.sql
+++ LeetCode/Frequency/183. Customers Who Never Order.sql
@@ -3,3 +3,21 @@ FROM Customers
 WHERE Id NOT IN (SELECT c.Id 
                  FROM Customers c 
                  JOIN Orders o ON c.Id = o.CustomerId);
+
+/*----------------------------------------------------*/
+
+SELECT name AS Customers
+FROM Customers c
+LEFT JOIN Orders o ON c.id=o.customerId
+WHERE o.customerId IS NULL
+
+
+/*----------------------------------------------------*/
+
+  
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (SELECT customerId FROM Orders )
+
+
+速度 1>3>2
