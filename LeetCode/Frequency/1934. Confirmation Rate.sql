cfc001b9696be18499035f1bb24e574a7d54f29b
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-15 20:50:59 -0700

    Update 1934. Confirmation Rate.sql

--- LeetCode/Frequency/1934. Confirmation Rate.sql
+++ LeetCode/Frequency/1934. Confirmation Rate.sql
@@ -4,3 +4,13 @@ FROM Signups s
 LEFT JOIN Confirmations c
 ON s.user_id = c.user_id
 GROUP BY s.user_id
+
+
+/*--------------------------------------------------------------------------*/
+
+SELECT s.user_id, IFNULL(confirmation_rate,0) AS confirmation_rate
+FROM Signups s
+LEFT JOIN (SELECT user_id, 
+           ROUND(SUM(CASE WHEN action='confirmed' THEN 1 END)/COUNT(user_id),2) AS confirmation_rate
+           FROM Confirmations GROUP BY 1) c
+ON s.user_id=c.user_id
