fb1a03450651df4e27ac459249b5720559fbee72
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-16 08:49:38 -0700

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- LeetCode/Frequency/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ LeetCode/Frequency/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -3,3 +3,11 @@ FROM Visits v
 LEFT JOIN Transactions t ON v.visit_id = t.visit_id
 WHERE t.transaction_id IS NULL
 GROUP BY v.customer_id
+
+
+
+/*---------------------------------------------------------------------*/
+SELECT customer_id, count(visit_id ) AS count_no_trans 
+FROM Visits
+WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
+GROUP BY 1
