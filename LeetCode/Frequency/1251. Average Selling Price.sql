4973c6463bbbe94ad6de32ffe159c4111a591eb8
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-17 13:43:52 -0700

    Update 1251. Average Selling Price.sql

--- LeetCode/Frequency/1251. Average Selling Price.sql
+++ LeetCode/Frequency/1251. Average Selling Price.sql
@@ -1,6 +1,5 @@
-SELECT DISTINCT p.product_id, 
+SELECT p.product_id, 
        ROUND((SUM(p.price * u.units))/(SUM(u.units)), 2) AS average_price
 FROM Prices p
-JOIN UnitsSold u ON p.product_id = u.product_id
-WHERE u.purchase_date BETWEEN p.start_date AND p.end_date
+LEFT JOIN UnitsSold u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
