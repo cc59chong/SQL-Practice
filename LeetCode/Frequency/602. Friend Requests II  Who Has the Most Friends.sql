672f9cc9750d0568453b8b090f5322f3a7c00172
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-18 00:28:17 -0700

    Update 602. Friend Requests II  Who Has the Most Friends.sql

--- LeetCode/Frequency/602. Friend Requests II  Who Has the Most Friends.sql
+++ LeetCode/Frequency/602. Friend Requests II  Who Has the Most Friends.sql
@@ -1,10 +1,10 @@
 select id1 as id, count(id2) as num
 from
 (select requester_id as id1, accepter_id as id2 
-from request_accepted
-union
+from RequestAccepted
+union all
 select accepter_id as id1, requester_id as id2 
-from request_accepted) tmp1
+from RequestAccepted) tmp1
 group by id1 
 order by num desc limit 1
 
