7f7fc6b2cba7492b3556b23c9d1e2d8ba7b35078
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-18 11:44:32 -0700

    Update 1341. Movie Rating.sql

--- LeetCode/Frequency/1341. Movie Rating.sql
+++ LeetCode/Frequency/1341. Movie Rating.sql
@@ -1,7 +1,36 @@
-SELECT id,
-       CASE WHEN P_id is NULL THEN 'Root'
-            WHEN id IN (SELECT DISTINCT P_id FROM tree) THEN 'Inner'
-            ELSE 'Leaf'
-       END AS Type
-FROM tree
-ORDER BY id
+(SELECT name AS results
+FROM MovieRating mr
+JOIN Users u ON mr.user_id=u.user_id
+GROUP BY mr.user_id
+ORDER BY COUNT(rating) DESC, name
+LIMIT 1)
+UNION ALL
+(SELECT title AS results
+FROM MovieRating mr
+JOIN Movies m ON mr.movie_id=m.movie_id
+WHERE LEFT(created_at,7) = '2020-02'
+GROUP BY mr.movie_id
+ORDER BY AVG(rating) DESC, title
+LIMIT 1)
+
+
+(
+SELECT name AS results
+FROM Users
+JOIN (SELECT user_id, COUNT(movie_id) AS num
+      FROM Movie_Rating
+      GROUP BY user_id) t1
+ON Users.user_id = t1.user_id
+ORDER BY t1.num DESC, name LIMIT 1
+)
+UNION ALL
+(
+SELECT title AS results
+FROM Movies
+JOIN (SELECT movie_id, AVG(rating) as rate
+      FROM Movie_Rating
+      WHERE MONTH(created_at) = 2 /*最好是LEFT(created_at,7) = '2020-02'*/
+      GROUP BY movie_id) b
+ON Movies.movie_id = b.movie_id
+ORDER BY b.rate DESC, title LIMIT 1
+)
