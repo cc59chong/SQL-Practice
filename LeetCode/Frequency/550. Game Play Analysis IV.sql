f4e875ee4c5148d1da86c059c8f2b106eec5fc7c
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-20 10:54:37 -0700

    Update 550. Game Play Analysis IV.sql

--- LeetCode/Frequency/550. Game Play Analysis IV.sql
+++ LeetCode/Frequency/550. Game Play Analysis IV.sql
@@ -16,10 +16,14 @@ FROM (
 
 
 
-/*------------------??????----------------------------------------------------*/ 
--- SELECT ROUND(COUNT(t2.player_id)/COUNT(t1.player_id),2) AS fraction
--- FROM
--- (SELECT player_id, MIN(event_date) AS first_login 
---  FROM Activity GROUP BY player_id) t1 
---  LEFT JOIN Activity t2
---  ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1
+/*----------------------------------------------------------------------------*/ 
+SELECT ROUND(
+COUNT(a.player_id)/(SELECT COUNT(DISTINCT player_id) FROM Activity),2
+) AS fraction  
+FROM Activity a
+LEFT JOIN 
+(SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id) f
+ON a.player_id=f.player_id
+WHERE DATEDIFF(a.event_date, f.first_login)=1
