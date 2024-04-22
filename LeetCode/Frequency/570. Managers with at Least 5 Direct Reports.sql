c6a48d1dc9eae3ed8ee1441002c6b4dca4239696
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-21 18:23:16 -0700

    Update 570. Managers with at Least 5 Direct Reports.sql

--- LeetCode/Frequency/570. Managers with at Least 5 Direct Reports.sql
+++ LeetCode/Frequency/570. Managers with at Least 5 Direct Reports.sql
@@ -4,3 +4,12 @@ WHERE Id IN (SELECT ManagerId
              FROM Employee
              GROUP BY ManagerId
              HAVING COUNT(Id) >= 5)
+
+/*--------------------------------------------*/
+SELECT e2.name
+FROM Employee e1, Employee e2
+WHERE e1.managerId=e2.id
+GROUP BY e1.managerId
+HAVING COUNT(e2.id) >=5
+
+速度 2>1
