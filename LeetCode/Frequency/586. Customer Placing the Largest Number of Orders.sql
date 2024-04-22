a17e33c242a08670f508d101b3c96d96544fe8dc
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-22 15:40:52 -0700

    Update 586. Customer Placing the Largest Number of Orders.sql

--- LeetCode/Frequency/586. Customer Placing the Largest Number of Orders.sql
+++ LeetCode/Frequency/586. Customer Placing the Largest Number of Orders.sql
@@ -4,7 +4,7 @@ FROM orders
 GROUP BY customer_number
 ORDER BY COUNT(order_number) DESC LIMIT 1;
 
-------------------------------------------------------
+/*------------------------------------------------------*/
 
 SELECT customer_number
 FROM orders
@@ -12,3 +12,15 @@ GROUP BY customer_number
 HAVING COUNT(order_number) >= ALL (SELECT COUNT(order_number) 
                                    FROM orders 
                                    GROUP BY customer_number)
+
+/*------------------------------------------------------*/
+  
+SELECT customer_number 
+FROM orders
+GROUP BY customer_number
+HAVING count(order_number) = (
+	SELECT count(order_number)
+	FROM orders
+	GROUP BY customer_number
+	ORDER BY count(order_number) DESC LIMIT 1
+)
