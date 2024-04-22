dc145db9dca8748705a8535a245a4d2df1386a43
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-22 15:43:00 -0700

    Update 586. Customer Placing the Largest Number of Orders.sql

--- LeetCode/Frequency/586. Customer Placing the Largest Number of Orders.sql
+++ LeetCode/Frequency/586. Customer Placing the Largest Number of Orders.sql
@@ -4,7 +4,9 @@ FROM orders
 GROUP BY customer_number
 ORDER BY COUNT(order_number) DESC LIMIT 1;
 
-/*------------------------------------------------------*/
+/*------------------------------------------------------
+Follow up: What if more than one customer has the largest number of orders, can you find all the customer_number in this case?*/
+
 
 SELECT customer_number
 FROM orders
