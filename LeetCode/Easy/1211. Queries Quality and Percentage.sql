f4e24ae5dcf2e359328c73b8e2c81fa7ee867262
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-22 16:25:41 -0700

    Update 1211. Queries Quality and Percentage.sql

--- LeetCode/Easy/1211. Queries Quality and Percentage.sql
+++ LeetCode/Easy/1211. Queries Quality and Percentage.sql
@@ -1,3 +1,14 @@
+
+SELECT query_name,
+      ROUND(AVG(rating/position),2) AS quality,
+      ROUND(IFNULL(SUM(CASE WHEN rating < 3 THEN 1 END)/COUNT(rating)*100,0)
+        ,2) AS poor_query_percentage 
+FROM Queries
+WHERE query_name IS NOT NULL /*in case quert_name is NULL*/
+GROUP BY 1
+
+/*---------------------------------------------------------------------*/
+
 SELECT query_name,
        ROUND((SUM(rating/position) / COUNT(query_name)), 2) AS quality,
        ROUND((SUM(IF (rating<3, 1, 0)) / COUNT(query_name) * 100), 2) AS poor_query_percentage
