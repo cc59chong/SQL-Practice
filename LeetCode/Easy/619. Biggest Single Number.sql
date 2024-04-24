1fe5f5242401f80fbe8122731b17791aa72cfdc7
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-24 12:16:58 -0700

    Update 619. Biggest Single Number.sql

--- LeetCode/Easy/619. Biggest Single Number.sql
+++ LeetCode/Easy/619. Biggest Single Number.sql
@@ -1,6 +1,27 @@
+
+SELECT IFNULL(MAX(num),NULL) AS num
+FROM
+(SELECT num
+FROM MyNumbers
+GROUP BY num
+HAVING COUNT(*) =1) t
+
+/*-------------------------------------------------*/
+
+
 SELECT IFNULL(NULL,MAX(num)) AS num
 FROM my_numbers
 WHERE num IN (SELECT num
               FROM my_numbers
               GROUP BY num
               HAVING COUNT(num) <= 1);
+
+/*-------------------------------------------------*/
+
+SELECT IFNULL(
+    (SELECT DISTINCT num
+     FROM MyNumbers
+     GROUP BY num
+     HAVING COUNT(num)=1
+     ORDER BY num DESC
+     LIMIT 1),NULL) AS num
