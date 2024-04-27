dddd12e682072848f637fe67abd8f44ac5feb47d
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-27 16:25:43 -0700

    Update 1082. Sales Analysis I.sql

--- LeetCode/Frequency/1082. Sales Analysis I.sql
+++ LeetCode/Frequency/1082. Sales Analysis I.sql
@@ -3,3 +3,15 @@ SELECT seller_id
 FROM Sales
 GROUP BY seller_id
 HAVING SUM(price) >= ALL (SELECT SUM(price) FROM Sales GROUP BY seller_id);
+
+/*--------------------------------------------------------------------------*/
+
+SELECT seller_id
+FROM Sales
+GROUP BY seller_id
+HAVING SUM(price) IN (SELECT MAX(price)
+                      FROM(SELECT SUM(price) AS price
+                           FROM Sales
+                           GROUP BY seller_id) t )
+
+速度 2>1
