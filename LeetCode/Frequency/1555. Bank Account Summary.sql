78c6c2963a1bf671182375055fc671c11aaba71f
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-26 23:18:44 -0700

    Update 1555. Bank Account Summary.sql

--- LeetCode/Frequency/1555. Bank Account Summary.sql
+++ LeetCode/Frequency/1555. Bank Account Summary.sql
@@ -1,3 +1,13 @@
+SELECT user_id,user_name,
+IFNULL(SUM(CASE WHEN a.user_id=b.paid_by THEN -amount ELSE amount END),0)+a.credit as credit,
+CASE WHEN IFNULL(SUM(CASE WHEN a.user_id=b.paid_by THEN -amount ELSE amount END),0)>=-a.credit THEN "No" ELSE "Yes" END as credit_limit_breached
+FROM Users as a
+LEFT JOIN Transactions as b
+ON a.user_id=b.paid_by OR a.user_id=b.paid_to
+GROUP BY a.user_id;
+
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+
 SELECT DISTINCT u.user_id,
        u.user_name,
        credit - IFNULL(pya,0) + IFNULL(pda,0) AS credit,
