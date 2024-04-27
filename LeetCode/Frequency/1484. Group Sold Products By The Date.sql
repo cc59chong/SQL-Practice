993fdb25ce6a59b88c27fd08148881ab8121811b
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-27 16:47:25 -0700

    Update 1484. Group Sold Products By The Date.sql

--- LeetCode/Frequency/1484. Group Sold Products By The Date.sql
+++ LeetCode/Frequency/1484. Group Sold Products By The Date.sql
@@ -1,5 +1,5 @@
-SELECT sell_date, 
-       COUNT(DISTINCT product) AS num_sold, 
-       GROUP_CONCAT(DISTINCT product) AS products
+SELECT sell_date,
+       COUNT(DISTINCT product) AS num_sold,
+       GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',' ) AS products
 FROM Activities
 GROUP BY sell_date;
