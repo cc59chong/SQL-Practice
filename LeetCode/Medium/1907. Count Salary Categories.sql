6badf6f793bb133c042bf5397bf13ab9b787347c
M.C <55506640+cc59chong@users.noreply.github.com>
2024-04-28 21:10:55 -0700

    Add files via upload

---
+++ LeetCode/Medium/1907. Count Salary Categories.sql
@@ -0,0 +1,20 @@
+SELECT 
+    'Low Salary' AS category,
+    SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS accounts_count
+FROM 
+    Accounts
+    
+UNION
+SELECT  
+    'Average Salary' category,
+    SUM(CASE WHEN income >= 20000 AND income <= 50000 THEN 1 ELSE 0 END) 
+    AS accounts_count
+FROM 
+    Accounts
+
+UNION
+SELECT 
+    'High Salary' category,
+    SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
+FROM 
+    Accounts
