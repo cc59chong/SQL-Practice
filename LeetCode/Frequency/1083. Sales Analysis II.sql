8b3039a130313e946221f92eca6e91bc46050f61
M.C <55506640+cc59chong@users.noreply.github.com>
2024-05-08 15:48:59 -0700

    Update 1083. Sales Analysis II.sql

--- LeetCode/Frequency/1083. Sales Analysis II.sql
+++ LeetCode/Frequency/1083. Sales Analysis II.sql
@@ -1,10 +1,21 @@
 SELECT          DISTINCT buyer_id
 FROM            Sales AS s
-LEFT JOIN       Product AS p  /*OR JOIN*/
-ON              s.product_id = p.product_id
+LEFT JOIN       Product AS p  /*OR JOIN*/ ON s.product_id = p.product_id
 WHERE           p.product_name = 'S8' AND 
                 buyer_id NOT IN (SELECT DISTINCT buyer_id 
                                  FROM            Sales AS s
                                  LEFT JOIN       Product AS p /*OR JOIN*/
                                  ON              s.product_id = p.product_id
                                  WHERE           p.product_name = 'iPhone');
+
+/*-----------------------------------------------------------------------------*/
+SELECT DISTINCT buyer_id
+FROM Sales
+WHERE buyer_id IN (SELECT s.buyer_id
+                     FROM Product p
+                     JOIN Sales s ON p.product_id=s.product_id
+                     WHERE product_name = 'S8')
+AND buyer_id NOT IN(SELECT s.buyer_id
+                      FROM Product p
+                      JOIN Sales s ON p.product_id=s.product_id
+                      WHERE product_name = 'iPhone')
