e94d6f2e4af8c20cb2e6d96bfd0a0b81901fd890
M.C <55506640+cc59chong@users.noreply.github.com>
2024-05-10 09:38:00 -0700

    Update 1173. Immediate Food Delivery I.sql

--- LeetCode/Easy/1173. Immediate Food Delivery I.sql
+++ LeetCode/Easy/1173. Immediate Food Delivery I.sql
@@ -1,3 +1,19 @@
+SELECT ROUND(
+    SUM(order_date=customer_pref_delivery_date)/COUNT(delivery_id)*100
+    ,2
+) AS immediate_percentage 
+FROM Delivery
+
+/*------------------------------------------------------------------*/
+
+  SELECT ROUND(
+    AVG(order_date=customer_pref_delivery_date)*100
+    ,2
+) AS immediate_percentage 
+FROM Delivery
+
+/*------------------------------------------------------------------*/
+  
 SELECT ROUND((SELECT COUNT(delivery_id) FROM Delivery
              WHERE order_date = customer_pref_delivery_date)
              /
