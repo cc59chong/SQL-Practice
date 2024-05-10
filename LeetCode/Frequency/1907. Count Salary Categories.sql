882a6d86231bd5693dc62dfec935a49fd2665a2b
M.C <55506640+cc59chong@users.noreply.github.com>
2024-05-10 16:19:16 -0700

    Update 1907. Count Salary Categories.sql

--- LeetCode/Frequency/1907. Count Salary Categories.sql
+++ LeetCode/Frequency/1907. Count Salary Categories.sql
@@ -18,3 +18,31 @@ SELECT
     SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
 FROM 
     Accounts
+
+/*----------------------------------------------------------------------*/
+    
+(select 
+"Low Salary" as category,
+(select count(*)  from Accounts where income < 20000) as accounts_count)
+union all
+(select 
+"Average Salary" as category,
+(select count(*) from Accounts where income >= 20000 and income <= 50000) as accounts_count)
+union all 
+(select 
+"High Salary" as category,
+(select count(*) from Accounts where income > 50000) as accounts_count)
+
+/*----------------------------------------------------------------------*/
+
+SELECT 'Low Salary' AS category, 
+       COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
+FROM Accounts
+UNION
+SELECT 'Average Salary' AS category, 
+       COUNT(CASE WHEN income>=20000 AND income <=50000 THEN 1 END) AS accounts_count
+FROM Accounts
+UNION
+SELECT 'High Salary' AS category, 
+       COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
+FROM Accounts
