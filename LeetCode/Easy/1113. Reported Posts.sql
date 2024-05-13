fd2a9770a545f43458c6ef943b36e1662c03d30d
M.C <55506640+cc59chong@users.noreply.github.com>
2024-05-13 13:33:29 -0700

    Update 1113. Reported Posts.sql

--- LeetCode/Easy/1113. Reported Posts.sql
+++ LeetCode/Easy/1113. Reported Posts.sql
@@ -2,3 +2,12 @@ SELECT extra AS report_reason, COUNT(DISTINCT post_id) AS report_count
 FROM Actions
 WHERE DATEDIFF('2019-07-05', action_date) = 1 AND action = 'report'
 GROUP BY extra;
+
+
+/*----------------------------------------------------------------------*/
+
+SELECT extra AS report_reason,
+       COUNT(DISTINCT post_id ) AS report_count 
+FROM Actions
+WHERE action_date = DATE_SUB('2019-07-05', INTERVAL 1 DAY) AND action='report'
+GROUP BY extra
