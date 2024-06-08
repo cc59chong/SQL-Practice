6db6611c27ba5b68487ddb674309518a3e4535ad
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-07 22:59:11 -0700

    Update 1777. Product's Price for Each Store.sql

--- LeetCode/Frequency/1777. Product's Price for Each Store.sql
+++ LeetCode/Frequency/1777. Product's Price for Each Store.sql
@@ -5,6 +5,14 @@ SELECT product_id,
 FROM Products
 GROUP BY product_id
 
+SELECT product_id, 
+       SUM(CASE WHEN store='store1' THEN price END) AS store1,
+       SUM(CASE WHEN store='store2' THEN price END) AS store2,
+       SUM(CASE WHEN store='store3' THEN price END) AS store3
+FROM Products
+GROUP BY product_id
+
+
 /* Pivot Table
 SELECT *
 FROM (
