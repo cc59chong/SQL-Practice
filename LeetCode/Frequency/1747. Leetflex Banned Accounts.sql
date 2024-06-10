87b5724f5374cd788d7a08e296c70ad4c0f752c5
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-10 14:42:46 -0700

    Update 1747. Leetflex Banned Accounts.sql

--- LeetCode/Frequency/1747. Leetflex Banned Accounts.sql
+++ LeetCode/Frequency/1747. Leetflex Banned Accounts.sql
@@ -1,3 +1,4 @@
+
 SELECT DISTINCT l1.account_id
 FROM LogInfo l1, LogInfo l2
 WHERE l1.account_id=l2.account_id 
@@ -19,3 +20,10 @@ WHERE
   l1.login <= l2.logout AND l2.login <= l1.logout
 
 速度1>2
+
+SELECT DISTINCT l1.account_id
+FROM LogInfo l1, LogInfo l2
+WHERE l1.account_id=l2.account_id
+   AND l1.ip_address!=l2.ip_address
+   AND l2.login<=l1.logout
+   AND l1.login<=l2.logout
