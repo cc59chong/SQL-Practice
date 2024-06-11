2f25ee59ec5b9e3f626e6f3b1cbe9d9703220359
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-11 10:34:51 -0700

    Update 550. Game Play Analysis IV.sql

--- LeetCode/Frequency/550. Game Play Analysis IV.sql
+++ LeetCode/Frequency/550. Game Play Analysis IV.sql
@@ -1,5 +1,14 @@
-
-
+SELECT ROUND(IFNULL(
+    SUM(CASE WHEN DATEDIFF(event_date,first_day)=1 THEN 1 END)/COUNT(DISTINCT a.player_id)
+    ,0),2) AS fraction
+FROM Activity a
+JOIN (SELECT player_id, MIN(event_date) AS first_day
+FROM Activity
+GROUP BY player_id) t
+ON a.player_id=t.player_id
+  
+/*------------------------------------------------------------------------------*/
+  
 SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity),2) as fraction 
 FROM Activity
 WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) 
@@ -26,4 +35,4 @@ GROUP BY player_id) f
 ON a.player_id=f.player_id
 WHERE DATEDIFF(a.event_date, f.first_login)=1
 
-速度 1>2>3
+速度 1>2>3>4
