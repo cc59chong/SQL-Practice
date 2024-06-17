8787004b57843e69c98c539ae092ac78d22d6c58
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-16 22:49:40 -0700

    Update 1327. List the Products Ordered in a Period.sql

--- LeetCode/Easy/1327. List the Products Ordered in a Period.sql
+++ LeetCode/Easy/1327. List the Products Ordered in a Period.sql
@@ -1,3 +1,15 @@
+SELECT p.product_name, t.unit
+FROM Products p
+JOIN (
+SELECT product_id, SUM(unit) AS unit
+FROM Orders
+WHERE LEFT(order_date,7)='2020-02'
+GROUP BY product_id) t
+ON p.product_id=t.product_id
+WHERE unit>=100
+
+/*-------------------------------------------------------------*/
+
 SELECT p.product_name, SUM(o.unit) AS unit
 FROM Products p
 RIGHT JOIN Orders o ON p.product_id = o.product_id
