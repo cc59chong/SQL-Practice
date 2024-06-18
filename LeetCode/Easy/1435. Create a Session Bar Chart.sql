aad03b0e7d42eb7dea62bf7b58d4c4b45ce82f67
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-18 09:35:55 -0700

    Update 1435. Create a Session Bar Chart.sql

--- LeetCode/Easy/1435. Create a Session Bar Chart.sql
+++ LeetCode/Easy/1435. Create a Session Bar Chart.sql
@@ -5,3 +5,17 @@ UNION
 SELECT '[10-15>' AS 'bin', SUM(CASE WHEN duration / 60 >= 10 AND duration / 60 < 15 THEN 1 ELSE 0 END) AS 'total' FROM Sessions
 UNION
 SELECT '15 or more' AS 'bin', SUM(CASE WHEN duration / 60 >= 15 THEN 1 ELSE 0 END) AS 'total' FROM Sessions;
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+SELECT '[0-5>' AS 'bin', SUM(duration/60 < 5) AS 'total'
+FROM Sessions
+UNION
+SELECT '[5-10>' AS 'bin', SUM(duration/60 >= 5 AND duration/60 < 10) AS 'total'
+FROM Sessions
+UNION
+SELECT '[10-15>' AS 'bin', SUM(duration/60 >= 10 AND duration/60 < 15) AS 'total'
+FROM Sessions
+UNION
+SELECT '15 or more' AS 'bin', SUM(duration/60 >= 15) AS 'total'
+FROM Sessions
