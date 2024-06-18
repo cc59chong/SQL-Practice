b702b152cb0e9797d98412558ca17dba5df54502
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-17 20:08:48 -0700

    Update 1241. Number of Comments per Post.sql

--- LeetCode/Easy/1241. Number of Comments per Post.sql
+++ LeetCode/Easy/1241. Number of Comments per Post.sql
@@ -2,4 +2,5 @@ SELECT a.sub_id AS post_id, COUNT(DISTINCT b.sub_id) AS number_of_comments
 FROM Submissions a
 LEFT JOIN Submissions b ON a.sub_id = b.parent_id
 WHERE a.parent_id IS NULL
-GROUP BY a.sub_id;
+GROUP BY a.sub_id
+ORDER BY post_id;
