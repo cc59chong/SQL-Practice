e29d820058bfe3e52e1fb23509103d1b90bf4ea6
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-17 19:51:06 -0700

    Update 1873. Calculate Special Bonus.sql

--- LeetCode/Frequency/1873. Calculate Special Bonus.sql
+++ LeetCode/Frequency/1873. Calculate Special Bonus.sql
@@ -2,3 +2,12 @@ SELECT employee_id,
        CASE WHEN employee_id%2=1 AND name NOT LIKE 'M%' THEN salary ELSE 0 END AS bonus
 FROM Employees
 ORDER BY 1
+
+/*---------------------------------------------------------------------------*/
+
+SELECT employee_id,
+       CASE WHEN employee_id%2!=0 AND LEFT(name,1)!='M' THEN salary
+            ELSE 0
+        END AS bonus
+FROM Employees
+ORDER BY employee_id
