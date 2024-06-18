db23ee9e010af338bf26cc606b630c5e91b923b3
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-17 20:30:18 -0700

    Update 1294. Weather Type in Each Country.sql

--- LeetCode/Easy/1294. Weather Type in Each Country.sql
+++ LeetCode/Easy/1294. Weather Type in Each Country.sql
@@ -1,3 +1,18 @@
+SELECT c.country_name, w.weather_type
+FROM Countries c
+JOIN (
+SELECT country_id,
+       CASE WHEN AVG(weather_state) <=15 THEN 'Cold'
+            WHEN AVG(weather_state) >=25 THEN 'Hot'
+            ELSE 'Warm'
+        END AS weather_type
+FROM Weather
+WHERE LEFT(day,7)='2019-11'
+GROUP BY country_id) w
+ON c.country_id=w.country_id
+
+/*-----------------------------------------------------------*/
+       
 SELECT c.country_name, 
        CASE WHEN AVG(w.weather_state) <= 15 THEN 'Cold' 
             WHEN AVG(w.weather_state) >= 25 THEN 'Hot'
