eeb26e56a93410a3595f0e1fad0f6cc5879bb41f
M.C <55506640+cc59chong@users.noreply.github.com>
2024-06-17 20:11:39 -0700

    Update 1241. Number of Comments per Post.sql

--- LeetCode/Easy/1241. Number of Comments per Post.sql
+++ LeetCode/Easy/1241. Number of Comments per Post.sql
@@ -4,3 +4,25 @@ LEFT JOIN Submissions b ON a.sub_id = b.parent_id
 WHERE a.parent_id IS NULL
 GROUP BY a.sub_id
 ORDER BY post_id;
+
+/*
+SELECT *
+FROM Submissions a
+LEFT JOIN Submissions b ON a.sub_id = b.parent_id
+WHERE a.parent_id IS NULL
+
+| sub_id | parent_id | sub_id | parent_id |
+| ------ | --------- | ------ | --------- |
+| 1      | null      | 9      | 1         |
+| 1      | null      | 4      | 1         |
+| 1      | null      | 3      | 1         |
+| 1      | null      | 3      | 1         |
+| 2      | null      | 10     | 2         |
+| 2      | null      | 5      | 2         |
+| 1      | null      | 9      | 1         |
+| 1      | null      | 4      | 1         |
+| 1      | null      | 3      | 1         |
+| 1      | null      | 3      | 1         |
+| 12     | null      | null   | null      |
+
+*/
