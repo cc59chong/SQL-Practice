30ea525aac83bece00c07c9b71821c7401da774c
M.C. <55506640+cc59chong@users.noreply.github.com>
2025-12-15 23:34:31 -0800

    Refactor warehouse volume calculation query
    Refactored SQL query to use LEFT JOIN and simplified volume calculation.

--- LeetCode/Easy/1571. Warehouse Manager.sql
+++ LeetCode/Easy/1571. Warehouse Manager.sql
@@ -1,6 +1,16 @@
+SELECT name AS warehouse_name,
+       SUM(units * Width * Length * Height) volume
+FROM Warehouse w
+LEFT JOIN Products p
+ON w.product_id = p.product_id
+GROUP BY name
+
+-----------------------------------------------------------------------------------
+
 SELECT w.name AS warehouse_name, SUM(w.units * t.volume) AS volume
 FROM Warehouse w
 JOIN (SELECT product_id, product_name, (Width * Length * Height) AS volume
       FROM Products) AS t
 ON w.product_id = t.product_id
 GROUP BY w.name
+
