9f008068d324141f8c525ac36e9e8d1001bba00b
M.C. <55506640+cc59chong@users.noreply.github.com>
2025-12-17 14:41:24 -0800

    Reorder and refine SQL queries for second highest salary

--- LeetCode/Frequency/176. Second Highest Salary.sql
+++ LeetCode/Frequency/176. Second Highest Salary.sql
@@ -1,12 +1,19 @@
-SELECT IFNULL(MAX(Salary), NULL) AS SecondHighestSalary
-FROM Employee
-WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee);
-
 SELECT
     IFNULL(
       (SELECT DISTINCT Salary
        FROM Employee
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
-    NULL) AS SecondHighestSalary
-;
+    NULL) AS SecondHighestSalary;
+
+
+
+SELECT IFNULL(MAX(Salary), NULL) AS SecondHighestSalary
+FROM Employee
+WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee);
+
+
+
+SELECT IFNULL(MAX(salary), NULL) AS SecondHighestSalary 
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
