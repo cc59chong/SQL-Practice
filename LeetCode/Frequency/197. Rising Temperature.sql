943cd70a97bbd5ea9cdaf98aa3d79eefe328bb0c
M.C. <55506640+cc59chong@users.noreply.github.com>
2025-12-17 22:39:37 -0800

    Refactor SQL query for rising temperature logic

--- LeetCode/Frequency/197. Rising Temperature.sql
+++ LeetCode/Frequency/197. Rising Temperature.sql
@@ -1,3 +1,15 @@
 SELECT b.id
 FROM Weather a, Weather b
 WHERE DATEDIFF(b.recordDate, a.recordDate) = 1 AND b.Temperature > a.Temperature;
+
+
+SELECT w2.id
+FROM Weather w1
+JOIN Weather w2 ON w2.recordDate = DATE_ADD(w1.recordDate, INTERVAL 1 DAY)
+WHERE w2.temperature > w1.temperature;
+
+
+SELECT w2.id
+FROM Weather w1
+JOIN Weather w2 ON w1.recordDate = DATE_SUB(w2.recordDate, INTERVAL 1 DAY)
+WHERE w2.temperature > w1.temperature;
